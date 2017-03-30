c9d5b21489d1c5a3ec87bfec802b31a24bd5815d
Brian Jinwright <bjinwright@qwigo.com>
2017-03-30 00:57:44 -0400

    updated descriptions in the setup.py

--- setup.py
+++ setup.py
@@ -10,16 +10,19 @@ LONG_DESCRIPTION = """
 Kev
 =======================
 
-K.E.V. (Keys, Extra Stuff, and Values) is a Python ORM for key-value stores. Currently supported
-backends are Redis and a S3/Redis hybrid backend.
+K.E.V. (Keys, Extra Stuff, and Values) is a Python ORM for key-value 
+stores and document databases based on Valley. Currently supported 
+backends are Redis, S3, DynamoDB and a S3/Redis hybrid backend.
 
 """
 
 setup(
     name='kev',
     version=version,
-    description="""K.E.V. (Keys, Extra Stuff, and Values) is a Python ORM for key-value stores. \
-        Currently supported backends are Redis and a S3/Redis hybrid backend.""",
+    description="""K.E.V. (Keys, Extra Stuff, and Values) is a Python 
+    ORM for key-value stores and document databases based on Valley. 
+    Currently supported backends are Redis, S3, DynamoDB and a 
+    S3/Redis hybrid backend.""",
     long_description=LONG_DESCRIPTION,
     classifiers=[
         "Programming Language :: Python",
