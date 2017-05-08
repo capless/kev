c3841a8447f1ed3409bce646fa56b16179d66357
Brian Jinwright <bjinwright@qwigo.com>
2017-05-07 23:12:13 -0400

    added dynamodb to setup.py keywords

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Environment :: Web Environment",
     ],
-    keywords='redis, s3',
+    keywords='redis, s3, dynamodb',
     author='Brian Jinwright',
     author_email='opensource@ipoots.com',
     maintainer='Brian Jinwright',
