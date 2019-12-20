36ce4319c5399e8ce32cdb8b59c4139a0faf137d
Benoit Brayer <brayer.benoit@gmail.com>
2019-12-20 15:22:53 +0100

    Add support to signature_version.

--- kev/backends/s3/db.py
+++ kev/backends/s3/db.py
@@ -15,7 +15,8 @@ class S3DB(DocDB):
                     '[-\W\w\s]+)/(?P<doc_id>[-\w]+):id:' \
                     '(?P<backend_id_b>[-\w]+):(?P<class_name_b>[-\w]+)$'
     session_kwargs = ['aws_secret_access_key', 'aws_access_key_id',
-                      'region_name', 'endpoint_url']
+                      'region_name', 'endpoint_url', 
+                      'signature_version']
 
     def __init__(self,**kwargs):
         #
@@ -25,10 +26,23 @@ class S3DB(DocDB):
         if 'endpoint_url' in session_kwargs.keys():
             endpoint_url =  session_kwargs['endpoint_url']   
             del(session_kwargs['endpoint_url'])
+        signature_version = None
+        if 'signature_version' in session_kwargs.keys():
+            signature_version = session_kwargs['signature_version']
+            del(session_kwargs['signature_version'])
         if len(session_kwargs.keys()) > 0:
             boto3.Session(**session_kwargs)
 
-        self._db = boto3.resource('s3', endpoint_url=endpoint_url)
+        if signature_version is not None:
+            self._db = boto3.resource(
+                's3',
+                endpoint_url=endpoint_url,
+                config=boto3.session.Config(
+                   signature_version=signature_version
+                )
+            )
+        else:
+            self._db = boto3.resource('s3', endpoint_url=endpoint_url)
         self.bucket = kwargs['bucket']
         self._indexer = self._db.Bucket(self.bucket)
 
