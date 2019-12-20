613e2c71c04704aacc62b7dbfdc19f48e9377ca0
Benoit Brayer <brayer.benoit@gmail.com>
2019-12-20 14:03:27 +0100

    Fix parse_requirements import for pip >= 10

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+try: # Fix for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 
